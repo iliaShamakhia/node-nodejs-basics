931d398cb9d1fb171a6e465c60a48407315d4698
iliaShamakhia <shamakhiailia@gmail.com>
2025-10-25 19:21:53 +0400

    commit untracked changes

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
   // Write your code here
+  try {
+    const rssVars = Object.entries(process.env)
+      .filter(([key]) => key.startsWith('RSS_'))
+      .map(([key, value]) => `${key}=${value}`);
+
+    if (rssVars.length > 0) {
+      console.log(rssVars.join('; '));
+    } else {
+      console.log('No RSS_ environment variables found.');
+    }
+  } catch (err) {
+    console.error('Error reading environment variables:', err.message);
+  }
 };
 
 parseEnv();
