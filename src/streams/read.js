d087728cca9458898cba59d3eead79062b835d3b
iliaShamakhia <shamakhiailia@gmail.com>
2025-10-25 20:03:41 +0400

    implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
   // Write your code here
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(currentDir, 'files', 'fileToRead.txt');
+  const stream = createReadStream(filePath);
+
+  stream.pipe(process.stdout);
+
+  stream.on('error', err => {
+    console.error('Error reading file:', err.message);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
   // Write your code here
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(currentDir, 'files', 'fileToWrite.txt');
+  const writableStream = createWriteStream(filePath);
+
+  process.stdin.pipe(writableStream);
+
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('data', chunk => {
+    if (chunk.trim() === 'exit') {
+      process.stdin.unpipe(writableStream);
+      writableStream.end();
+    }
+  });
+
+  writableStream.on('finish', () => {
+    console.log('Data has been written to fileToWrite.txt');
+  });
+
+  writableStream.on('error', err => {
+    console.error('Error writing to file:', err.message);
+  });
+
+  console.log("type 'exit' and press Enter to finish");
 };
 
 await write();
