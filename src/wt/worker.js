4bf47a24b1f2a5274ec595b0631bf18406dfc2a6
iliaShamakhia <shamakhiailia@gmail.com>
2025-10-27 11:39:34 +0400

    minor refactoring

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,18 +3,12 @@ import { parentPort } from 'node:worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-parentPort.on('message', (n) => {
-  sendResult(n);
-});
-
 const sendResult = (n) => {
   // This function sends result of nthFibonacci computations to main thread
-  try {
+  parentPort.on('message', (n) => {
     let result = nthFibonacci(n);
     parentPort.postMessage({ status: 'resolved', data: result });
-  } catch (error) {
-    parentPort.postMessage({ status: 'error', data: null });
-  }
+  });
 };
 
-//sendResult(); commented this line because it runs before the 'message' event from parent fires, so it sends result 0 back to parent.
+sendResult();
